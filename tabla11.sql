6b019e2d37b668d2996e4467770512f19af219f8
Esteban Salinas <estebansalinas@madalenaenergy.local>
2022-11-01 12:31:10 -0300

    listo

---
+++ tabla11.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-11-2022 a las 16:27:17
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tp unidad 11`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tabla11`
+--
+
+CREATE TABLE `tabla11` (
+  `ID` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `apellido` varchar(40) NOT NULL,
+  `edad` tinyint(2) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp(),
+  `provincia` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tabla11`
+--
+
+INSERT INTO `tabla11` (`ID`, `nombre`, `apellido`, `edad`, `fecha`, `provincia`) VALUES
+(1, 'Juan', 'Perez', 45, '2022-11-01 15:07:46', 'Buenos Aires'),
+(2, 'Jose', 'Gonzales', 55, '2022-11-01 18:13:22', 'Buenos Aires'),
+(3, 'Esteban', 'Martin', 33, '2022-11-02 10:22:08', 'Capital Federal'),
+(4, 'Nicolas', 'Gimenez', 25, '2022-12-28 14:22:08', 'Mendoza'),
+(5, 'Sergio', 'Azul', 38, '2022-08-16 21:52:58', 'Tucumán');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tabla11`
+--
+ALTER TABLE `tabla11`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tabla11`
+--
+ALTER TABLE `tabla11`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
